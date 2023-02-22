274e63b2e75b22e98824d44d7c7c6239440caa84
f3liz <125415218+f3liz@users.noreply.github.com>
2023-02-21 16:26:29 -0800

    Add files via upload

---
+++ docs/Assignment06_Starter.py
@@ -0,0 +1,207 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment 06
+# Description: Working with functions in a class,
+#              When the program starts, load each "row" of data
+#              in "ToDoToDoList.txt" into a python Dictionary.
+#              Add the each dictionary "row" to a python list "table"
+# ChangeLog (Who,When,What):
+# RRoot,1.1.2030,Created started script
+# FChen,2.21.2023,Modified code to complete assignment 06
+# ---------------------------------------------------------------------------- #
+
+# Data ---------------------------------------------------------------------- #
+# Declare variables and constants
+file_name_str = "ToDoFile.txt"  # The name of the data file
+file_obj = None  # An object that represents a file
+row_dic = {}  # A row of data separated into elements of a dictionary {Task,Priority}
+table_lst = []  # A list that acts as a 'table' of rows
+choice_str = ""  # Captures the user option selection
+
+
+# Processing  --------------------------------------------------------------- #
+class Processor:
+    """  Performs Processing tasks """
+
+    @staticmethod
+    def read_data_from_file(file_name, list_of_rows):
+        """ Reads data from a file into a list of dictionary rows
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        list_of_rows.clear()  # clear current data
+        file = open(file_name, "r")
+        for line in file:
+            task, priority = line.split(",")
+            row = {"Task": task.strip(), "Priority": priority.strip()}
+            list_of_rows.append(row)
+        file.close()
+        return list_of_rows
+
+    @staticmethod
+    def add_data_to_list(task, priority, list_of_rows):
+        """ Adds data to a list of dictionary rows
+
+        :param task: (string) with name of task:
+        :param priority: (string) with name of priority:
+        :param list_of_rows: (list) you want to add more data to:
+        :return: (list) of dictionary rows
+        """
+        # TODO: Add Code Here!
+        dicRow = {"Task": str(task).strip(), "Priority": str(priority).strip()}
+        list_of_rows += [dicRow] # will add the data to the list
+
+        return list_of_rows
+
+    @staticmethod
+    def remove_data_from_list(task, list_of_rows):
+        """ Removes data from a list of dictionary rows
+
+        :param task: (string) with name of task:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        # TODO: Add Code Here!
+        for row in list_of_rows:
+            if row["Task"].lower() == task.lower():
+                list_of_rows.remove(row) # removes the specified task
+                print("Task has now been removed!") # only prints if the task is found and removed
+                break
+            else:
+                print("Task not found.") # prints if task not found
+        return list_of_rows
+
+    @staticmethod
+    def write_data_to_file(file_name, list_of_rows):
+        """ Writes data from a list of dictionary rows to a File
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        # TODO: Add Code Here!
+        objFile = open(file_name, "w")
+        for row in list_of_rows:
+            objFile.write(str(row["Task"]) + "," + str(row["Priority"]) + "\n") # writes the new data to the file
+        objFile.close()
+        return list_of_rows
+
+    @staticmethod
+    def create_new_file():
+        objFile = open(file_name_str, "w")  # to create a new file if one isn't found
+        objFile.close()
+
+# Presentation (Input/Output)  -------------------------------------------- #
+
+
+class IO:
+    """ Performs Input and Output tasks """
+
+    @staticmethod
+    def output_menu_tasks():
+        """  Display a menu of choices to the user
+
+        :return: nothing
+        """
+        print('''
+        Menu of Options
+        1) Add a new Task
+        2) Remove an existing Task
+        3) Save Data to File        
+        4) Exit Program
+        ''')
+        print()  # Add an extra line for looks
+
+    @staticmethod
+    def input_menu_choice():
+        """ Gets the menu choice from a user
+
+        :return: string
+        """
+        choice = str(input("Which option would you like to perform? [1 to 4] - ")).strip()
+        print()  # Add an extra line for looks
+        return choice
+
+    @staticmethod
+    def output_current_tasks_in_list(list_of_rows):
+        """ Shows the current Tasks in the list of dictionaries rows
+
+        :param list_of_rows: (list) of rows you want to display
+        :return: nothing
+        """
+        print()
+        print("******* The current tasks ToDo are: *******")
+        for row in list_of_rows:
+            print(row["Task"] + " (" + row["Priority"] + ")")
+        print("*******************************************")
+        print()  # Add an extra line for looks
+
+    @staticmethod
+    def input_new_task_and_priority():
+        """  Gets task and priority values to be added to the list
+
+        :return: (string, string) with task and priority
+        """
+        print("Please enter a task and it's priority from 1 (high) to 5(low)")
+        task = str(input("Name of task: "))
+        while (True):
+            priority = int(input("Priority of the task: "))
+            if priority not in range(1, 6):  # if user input isn't 1-5 it'll ask them to choose 1-5
+                print("Please choose from 1 being high priority to 5 being lowest priority")
+            else:
+                break
+        priority = str(priority) # changes the int back to string
+        return(task, priority)
+        pass  # TODO: Add Code Here!
+
+    @staticmethod
+    def input_task_to_remove():
+        """  Gets the task name to be removed from the list
+
+        :return: (string) with task
+        """
+        task = str(input("Name of the task you'd like removed: ")) # to store the name of task user wants to remove
+        return(task)
+        pass  # TODO: Add Code Here!
+
+# Main Body of Script  ------------------------------------------------------ #
+
+
+# Step 1 - When the program starts, Load data from ToDoFile.txt.
+try:
+    Processor.read_data_from_file(file_name=file_name_str, list_of_rows=table_lst)  # read file data
+except:
+    print('''
+    ToDoList.exe file currently not found
+    One has now been created for you
+    ''')  # lets users know they don't have the file
+    Processor.create_new_file()
+# Step 2 - Display a menu of choices to the user
+while (True):
+    # Step 3 Show current data
+    IO.output_current_tasks_in_list(list_of_rows=table_lst)  # Show current data in the list/table
+    IO.output_menu_tasks()  # Shows menu
+    choice_str = IO.input_menu_choice()  # Get menu option
+
+    # Step 4 - Process user's menu choice
+    if choice_str.strip() == '1':  # Add a new Task
+        task, priority = IO.input_new_task_and_priority()
+        table_lst = Processor.add_data_to_list(task=task, priority=priority, list_of_rows=table_lst)
+        continue  # to show the menu
+
+    elif choice_str == '2':  # Remove an existing Task
+        task = IO.input_task_to_remove()
+        table_lst = Processor.remove_data_from_list(task=task, list_of_rows=table_lst)
+        continue  # to show the menu
+
+    elif choice_str == '3':  # Save Data to File
+        table_lst = Processor .write_data_to_file(file_name=file_name_str, list_of_rows=table_lst)
+        print("Data Saved!")
+        continue  # to show the menu
+
+    elif choice_str == '4':  # Exit Program
+        print("Goodbye!")
+        break  # by exiting loop
+    else:
+        print("Please choose from options 1-4!!!")
